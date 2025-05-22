7cb2d6c97c8587470919227b608e60e1c436338e
vitorSchlickmann <vitorsluciano@gmail.com>
2025-05-22 20:33:55 -0300

    fix: Ajuste das convenções das classes criadas até então para a convenção de código K&R (Kernighan & Richie Style) ou chaves na mesma linha

--- backend/database/migrations/2025_05_22_221633_create_colaboradores_table.php
+++ backend/database/migrations/2025_05_22_221633_create_colaboradores_table.php
@@ -4,10 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
+return new class extends Migration {
+
+    public function up(): void {
         Schema::create('colaboradores', function(BluePrint $table) {
              $table->id();
              $table->string('código')->unique();
@@ -21,8 +20,8 @@ return new class extends Migration
              $table->timestamps();
         });
     }
-    public function down(): void
-    {
+
+    public function down(): void {
         Schema::dropIfExists('colaboradores');
     }
 };
